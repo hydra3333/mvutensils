853bafd4f02d33f9dff7905c669469d7a9cb15e8
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2026-07-14 13:03:31 +0200

    Check block count when loading serialized analysis data

--- src/MotionBlockPyramid.cpp
+++ src/MotionBlockPyramid.cpp
@@ -1463,24 +1463,23 @@ void MotionBlockPyramid::LoadFrameData(const VSFrame *srcFrame, bool loadVectors
     if (xRatioUV < 1 || yRatioUV < 1 || xRatioUV > 2 || yRatioUV > 2 || nRealWidth > nWidth || nRealHeight > nHeight || nVPadding < 0 || nHPadding < 0
         || nRealHeight < 1 || nRealWidth < 1 || nBlkSizeX < 2 || nBlkSizeY < 2 || nOverlapX < 0 || nOverlapY < 0
         || nOverlapX > nBlkSizeX / 2 || nOverlapY > nBlkSizeY / 2 || nLevelCount < 1 || (nPel != 1 && nPel != 2 && nPel != 4)
-        || bitsPerSample < 8 || (bitsPerSample > 16 && bitsPerSample != 32))
+        || bitsPerSample < 8 || (bitsPerSample > 16 && bitsPerSample != 32) || nBlkX < 1 || nBlkY < 1)
         return;
 
     if (loadVectors) {
         std::string vectorsProp = prefix + "AnalysisVectors";
         std::string sadProp = prefix + "AnalysisSAD";
 
-        pyramidLevels.resize(1);
-        pyramidLevels[0].Initialize(nBlkX, nBlkY, nBlkSizeX, nBlkSizeY, nPel, 0, true, chroma, nOverlapX, nOverlapY, xRatioUV, yRatioUV, bitsPerSample);
-
         int vectSize = vsapi->mapNumElements(props, vectorsProp.c_str());
         int sadSize = vsapi->mapNumElements(props, sadProp.c_str());
 
-        if (vectSize != sadSize || vectSize != nBlkX * nBlkY) {
-            pyramidLevels[0].vectors.clear();
+        if (vectSize != sadSize || static_cast<int64_t>(vectSize) != static_cast<int64_t>(nBlkX) * nBlkY) {
             state = State::MetadataOnly;
             return;
         }
+
+        pyramidLevels.resize(1);
+        pyramidLevels[0].Initialize(nBlkX, nBlkY, nBlkSizeX, nBlkSizeY, nPel, 0, true, chroma, nOverlapX, nOverlapY, xRatioUV, yRatioUV, bitsPerSample);
 
         const int64_t *vectData = vsapi->mapGetIntArray(props, vectorsProp.c_str(), nullptr);
         const int64_t *sadData = vsapi->mapGetIntArray(props, sadProp.c_str(), nullptr);
